dcdad0186e0be21f330d6f2f6f32e13abb9c744f
Andros Fenollosa <andros@fenollosa.email>
2019-03-28 00:38:26 +0100

    build

--- buildAndroidStore.sh
+++ buildAndroidStore.sh
@@ -9,9 +9,9 @@ cd build
 cordova -d build android --release
 
 # Mueve a la raíz el apk
-mv platforms/android/build/outputs/apk/android-release-unsigned.apk .
+mv platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk android-release-unsigned.apk
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../app.keystore android-release-unsigned.apk idecrea
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore idecrea.jks android-release-unsigned.apk idecrea
 
 # Alinea
 zipalign 4 android-release-unsigned.apk android-release-compress-unsigned.apk
@@ -20,7 +20,7 @@ zipalign 4 android-release-unsigned.apk android-release-compress-unsigned.apk
 rm android-release-unsigned.apk
 
 # Mover al directorio raiz
-mv android-release-compress-unsigned.apk ../final.apk
+mv android-release-compress-unsigned.apk final.apk
 
 # Te avisa 
 echo '-------------------------------------------------'
